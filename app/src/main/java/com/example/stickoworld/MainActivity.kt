b46cdcd98d173f0bf3c086c2f715839f9f36a50a
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-04-10 09:52:51 +0530

    lots of changes

--- app/src/main/java/com/example/stickoworld/MainActivity.kt
+++ app/src/main/java/com/example/stickoworld/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.stickoworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }, 8000) // 8000 is the delayed time in milliseconds.
+
+
     }
 }
