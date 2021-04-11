4a29f076aec32a3dcc80b0a0b9b5c07374f0f85a
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-04-11 11:31:20 +0530

    commit3

--- app/src/main/java/com/example/stickoworld/MainActivity2.kt
+++ app/src/main/java/com/example/stickoworld/MainActivity2.kt
@@ -1,20 +1,18 @@
 package com.example.stickoworld
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 
-
+@Suppress("DEPRECATION")
 class MainActivity2 : AppCompatActivity() {
 
     companion object {
@@ -23,7 +21,6 @@ class MainActivity2 : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
@@ -88,5 +85,6 @@ class MainActivity2 : AppCompatActivity() {
                 }
             }
     }
+
 
 }
