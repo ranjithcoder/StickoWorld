58e85ef0045a8f0fc4cfdcf627af83ff318906cf
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-04-16 21:42:32 +0530

    new sticker pack layout added

--- app/src/main/java/com/example/stickoworld/AboutDialogFragment.kt
+++ app/src/main/java/com/example/stickoworld/AboutDialogFragment.kt
@@ -6,18 +6,18 @@ import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
-
-class AboutDialogue : DialogFragment() {
+class AboutDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val alertDialog: AlertDialog = AlertDialog.Builder(
-            activity!!
+            Objects.requireNonNull(activity)!!
         )
             .setView(R.layout.about_layout)
             .create()
         alertDialog.show()
-        val btn = alertDialog.findViewById<AppCompatButton>(R.id.btnRound)
-        btn!!.setOnClickListener { v: View? -> alertDialog.dismiss() }
+        val btn = alertDialog.findViewById<AppCompatButton>(R.id.btnRound)!!
+        btn.setOnClickListener { v: View? -> alertDialog.dismiss() }
         return alertDialog
     }
 }
---
+++ app/src/main/java/com/example/stickoworld/NewStickerPackActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stickoworld
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewStickerPackActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newsticker_pack)
+    }
+
+}
